4f7eea8e59bbaf6acb18ff2b516a70ae43c41938
Ashutosh Jangra <ashutoshjangra603@gmail.com>
2020-08-02 14:52:11 +0530

    additional

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -3,9 +3,9 @@ const User = require('../models/user');
 
 exports.getAll = (req , res) => {
     
-    const user = res.locals.user ;
+    // const user = res.locals.user ;
     // console.log(user) ;
-    Project.find({user:user._id} , (err , foundProjects) => {
+    Project.find({} , (err , foundProjects) => {
         if(err){
             return res.json({"error" : "finding all projects"});
         }
@@ -17,6 +17,7 @@ exports.getAll = (req , res) => {
 
 exports.getBySlug =  (req , res) => {
    const slug = req.params.slug;
+   // console.log(req.headers.url);
   
     Project.find({folder:slug} , (err , foundProject) => {
        if(err){
@@ -27,10 +28,23 @@ exports.getBySlug =  (req , res) => {
    })
 };
 
+exports.getData =  (req , res) => {
+   // const slug = req.params.slug;
+   const link = req.headers.url;
+  
+    Project.find({title:link} , (err , foundProject) => {
+       if(err){
+           return res.json({"error" : "in find by slug"});
+       }
+       
+       return res.json(foundProject);
+   })
+};
+
 exports.createProject = async (req , res) => {
     const projectData = req.body ;
-    const user = res.locals.user ;
-    const newProject = new Project({...projectData, user:user._id}) ;
+    // const user = res.locals.user ;
+    const newProject = new Project({...projectData}) ;
 
     await Project.init();
     await Project.create(newProject , (err , createdProject) => {
