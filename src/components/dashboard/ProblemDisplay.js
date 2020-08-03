ead794b53cbc55abf58ce40b7d0e986460f6acd6
Chaitanya Kashyap <55341996+chaitanya-kashyap@users.noreply.github.com>
2020-08-03 15:14:21 +0530

    Update ProblemDisplay.js

--- src/components/dashboard/ProblemDisplay.js
+++ src/components/dashboard/ProblemDisplay.js
@@ -35,8 +35,8 @@ class ProblemDisplay extends Component {
 
 		return (
 
-			<Link className="no-underline mb5" to={`/`}>
-				<div className=" br4  flex flex-column justify-between mb2  bg-white" style={{width:"45vw" }}
+			<Link className="no-underline p-d mb5" to={`/`}>
+				<div className=" br4  flex flex-column justify-between mb2  bg-white">
 						{image 	? <img src={image} alt=""/> 
 								: <h2 className="f7  pa4 pv5 pv5-ns ma0  br4 black-80" style={{fontSize:'0.85em', background:'#00000009'}}>{text}</h2>
 						}						
